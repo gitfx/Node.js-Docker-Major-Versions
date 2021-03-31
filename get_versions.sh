17a4c1a8c03313c6d14be3c42c92e5b5ed985651
B1nj0y <idegorepl@gmail.com>
2021-03-31 13:22:41 +0800

    Update get_versions.sh

--- get_versions.sh
+++ get_versions.sh
@@ -2,4 +2,4 @@
 
 echo "# Node.js Docker image major versions"
 
-wget -q https://registry.hub.docker.com/v1/repositories/node/tags -O - | jq ".[].name" | sort -t "." -k1,1n -k2,2n -k3,3n | perl -nle 'print "* $1" if /^"(\d+\.[\d+\.?]+)"/' 
+wget -q https://registry.hub.docker.com/v1/repositories/node/tags -O - | jq ".[].name" | perl -nle 'print $1 if /^"(\d+\.[\d+\.?]+)"/' | sort -t "." -k1,1n -k2,2n -k3,3n | sed 's/^/\* /'
